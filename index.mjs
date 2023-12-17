fa613c9b0806d37cf0d690fb3f8f25325777f143
Luiz Souza <souza_luiz@live.com>
2023-12-17 16:51:06 -0300

    function authorizer

---
+++ index.mjs
@@ -0,0 +1,54 @@
+import { DynamoDBClient, GetItemCommand, UpdateItemCommand } from "@aws-sdk/client-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
+
+const client = new DynamoDBClient({ region: "us-west-2" });
+
+export const handler = async (event, context) => {
+
+
+    const exists = await getDynamoToken(event.authorizationToken);
+
+
+    if (exists) {
+        return generatePolicy('user', 'Allow', event.methodArn)
+    }
+
+    return generatePolicy('user', 'Deny', event.methodArn)
+};
+
+function generatePolicy(principalId, effect, resource) {
+    return {
+        'principalId': principalId,
+        'policyDocument': {
+            'Version': '2012-10-17',
+            'Statement': [{
+                'Action': 'execute-api:Invoke',
+                'Effect': effect,
+                'Resource': resource
+            }]
+        }
+    };
+}
+
+async function getDynamoToken(authToken) {
+    const input = {
+        Key: {
+            token: {
+                S: authToken,
+            },
+        },
+        TableName: "usuariosLogados",
+    };
+
+    const command = new GetItemCommand(input);
+    const dynamoResponse = await client.send(command);
+
+    if (dynamoResponse.Item === undefined) {
+        return false;
+    }
+
+    return true
+    //   const dynamoUnmarshall = unmarshall(dynamoResponse.Item);
+
+    //   return dynamoUnmarshall;
+}
